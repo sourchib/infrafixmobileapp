c960fff6c2ab005344647ac33c6228147e34945a
Muchammad Muchib Zainul Fikry <muchammadmuchib@gmail.com>
2025-12-12 23:37:16 +0800

    first commit

---
+++ src/main/java/com/mobile/infrafixapp/InfrafixappApplication.java
@@ -0,0 +1,13 @@
+package com.mobile.infrafixapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrafixappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfrafixappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobile/infrafixapp/InfrafixappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobile.infrafixapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrafixappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
